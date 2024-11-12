93a46064021ab6078a7d23c1468d2be9db852374
DevVictor19 <antoniovictor12@live.com>
2024-11-12 11:38:58 -0300

    feat: implement base mongo paginated repository

---
+++ src/shared/infra/repositories/base-paginated-mongo.repository.ts
@@ -0,0 +1,62 @@
+import { Model, SortOrder } from 'mongoose';
+import {
+  BasePaginatedRepository,
+  Page,
+  Params,
+} from 'src/shared/domain/repositories/base-paginated-repository.contract';
+
+import { BaseMongoRepository } from './base-mongo.repository';
+
+export abstract class BasePaginatedMongoRepository<T extends object>
+  extends BaseMongoRepository<T>
+  implements BasePaginatedRepository<T>
+{
+  constructor(model: Model<T>) {
+    super(model);
+  }
+
+  async findAllPaginated({
+    page = 0,
+    size = 10,
+    sort,
+    sortBy,
+    ...filters
+  }: Params<T>): Promise<Page<T>> {
+    if (isNaN(page)) page = 0;
+    if (isNaN(size)) size = 10;
+
+    const sortConfig = { [sortBy]: sort === 'ASC' ? 1 : -1 } as {
+      [key: string]: SortOrder;
+    };
+
+    const filterConfig: Record<string, any> = {};
+
+    Object.keys(filters).forEach((key) => {
+      if (filters[key as keyof typeof filters] !== undefined) {
+        filterConfig[key] = filters[key as keyof typeof filters];
+      }
+    });
+
+    const [totalElements, content] = await Promise.all([
+      this.model.countDocuments(filterConfig),
+      this.model
+        .find(filterConfig)
+        .sort(sortConfig)
+        .skip(page * size)
+        .limit(size)
+        .exec(),
+    ]);
+
+    const totalPages = Math.ceil(totalElements / size);
+
+    return {
+      content,
+      page: {
+        size,
+        number: page,
+        totalElements,
+        totalPages,
+      },
+    };
+  }
+}
