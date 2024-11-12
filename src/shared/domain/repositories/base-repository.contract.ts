ad842974afb9d29dc814c8573b4bcd8958a38022
DevVictor19 <antoniovictor12@live.com>
2024-11-12 11:37:34 -0300

    refactor: pass partial of fields on update

--- src/shared/domain/repositories/base-repository.contract.ts
+++ src/shared/domain/repositories/base-repository.contract.ts
@@ -2,6 +2,6 @@ export abstract class BaseRepository<T extends object> {
   abstract insert(entity: T): Promise<void>;
   abstract findById(id: string): Promise<T | null>;
   abstract findAll(): Promise<T[]>;
-  abstract update(id: string, entity: T): Promise<void>;
+  abstract update(id: string, fields: Partial<T>): Promise<void>;
   abstract delete(id: string): Promise<void>;
 }
