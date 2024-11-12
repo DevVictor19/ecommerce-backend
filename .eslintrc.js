13e918f2c629a5280534cc56b84cad9fcd11f651
DevVictor19 <antoniovictor12@live.com>
2024-11-12 13:56:50 -0300

    chore: add no-console eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
 };
