8255ee44f5af42a953f6795f29416fe27ef3753d
DevVictor19 <antoniovictor12@live.com>
2024-11-12 11:38:33 -0300

    refactor: fix update method and make model protected

--- src/shared/infra/repositories/base-mongo.repository.ts
+++ src/shared/infra/repositories/base-mongo.repository.ts
@@ -1,10 +1,10 @@
 import { Model } from 'mongoose';
 import { BaseRepository } from 'src/shared/domain/repositories/base-repository.contract';
 
-export class BaseMongoRepository<T extends object>
+export abstract class BaseMongoRepository<T extends object>
   implements BaseRepository<T>
 {
-  constructor(private model: Model<T>) {}
+  constructor(protected model: Model<T>) {}
 
   async insert(entity: T): Promise<void> {
     const model = new this.model(entity);
@@ -19,8 +19,8 @@ export class BaseMongoRepository<T extends object>
     return this.model.find();
   }
 
-  async update(id: string, entity: T): Promise<void> {
-    await this.model.findByIdAndUpdate(id, entity);
+  async update(id: string, fields: Partial<T>): Promise<void> {
+    await this.model.findByIdAndUpdate({ _id: id }, fields);
   }
 
   async delete(id: string): Promise<void> {
